6d7e50cab580ae6b9f47538c28a4bfcb814e18e9
Trey Dockendorf <tdockendorf@osc.edu>
2019-05-24 14:36:06 -0400

    Use PDK

--- spec/unit/facter/kernel_arguments_spec.rb
+++ spec/unit/facter/kernel_arguments_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 
 describe 'kernel_arguments fact' do
   before :each do
-    Facter.fact(:kernel).stubs(:value).returns('Linux')
+    allow(Facter.fact(:kernel)).to receive(:value).and_return('Linux')
   end
 
   it "should return output from /proc/cmdline" do
     expected_value = my_fixture_read('cmdline1')
-    Facter::Util::Resolution.expects(:exec).with('cat /proc/cmdline 2>/dev/null').returns(expected_value)
+    allow(Facter::Util::Resolution).to receive(:exec).with('cat /proc/cmdline 2>/dev/null').and_return(expected_value)
     expect(Facter.fact(:kernel_arguments).value).to eq(expected_value)
   end
 end
